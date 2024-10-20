1ada26400c4a2433863ef49852dcbd56f5d8658e
Shadab54 <shadabsiddiqui547@gmail.com>
2024-10-21 00:26:26 +0530

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Headerbox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Yazdan",
+    lastName: "  Siddiqui",
+    email: "yazdan@gmail.com",
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Headerbox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and managed your account and Transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:1250.35},{currentBalance:3512.12}]} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
